6dea61c1884089509198b557c01eae1fd69a661e
headsink <headsink@gmail.com>
2022-07-21 19:51:05 +0800

    new layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import AddTask from "./components/AddTask";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AddTask />
+      <Tasks />
     </div>
   );
 }
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,6 +3,16 @@ import React from 'react'
 const AddTask = () => {
   return (
     <form>
+      <div>
+        <label>TASK: </label>
+        <input type="text" placeholder='Add Task' />
+      </div>
+      <div>
+        <label>Set Reminder: </label>
+        <input type="checkbox" name="" id="" />
+      </div>
+
+      <input type="submit" value="Save" />
     
     </form>
   )
